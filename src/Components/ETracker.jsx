9b965ae6e301810c296ad08f88de72c0a29768b0
Kishor B K <kishorbkgowda36@gmail.com>
2024-04-03 17:49:00 +0530

    Minor changes made

--- src/Components/ETracker.jsx
+++ src/Components/ETracker.jsx
@@ -58,55 +58,55 @@ function ETracker() {
   };
 
   return (
-    <div className="w-screen bg-stone-100 h-screen flex justify-center items-center">
-      <div className="min-w-[380px] mx-auto text-center flex flex-col gap-2 p-2 border-[2px] border-gray-400">
-        <h1 className="lg:text-2xl font-bold">Expense Tracker</h1>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="glass min-w-[400px] mx-auto text-center flex flex-col gap-2 py-2 px-4 border-[2px] border-gray-400">
+        <h1 className="lg:text-2xl font-bold text-gray-400">Expense Tracker</h1>
         <div className="text-left mt-5">
-          <h3 className="md:text-[0.8rem] font-semibold">YOUR BALANCE</h3>
-          <h3 className="md:text-[1.5rem] font-semibold">
+          <h3 className="md:text-[0.9rem] font-semibold text-gray-400">YOUR BALANCE</h3>
+          <h3 className="md:text-[1.5rem] font-semibold text-gray-400">
             {bal === 0 ? "$0.00" : "$"+bal+".00"}
           </h3>
         </div>
-        <div className="flex bg-white p-4 border-b-black border-solid border-2">
+        <div className="flex glass p-4 border-solid border-2">
           <div className="flex-1">
-            <h2 className="text-[0.9rem] font-bold">INCOME</h2>
-            <h2 className="text-[1.1rem] text-green-600 font-bold">
+            <h2 className="text-[0.9rem] font-bold text-gray-400 ">INCOME</h2>
+            <h2 className="text-[1.1rem] text-green-400 font-bold">
               {income === 0 ? "$0.00" : "$"+income+".00"}
             </h2>
           </div>
           <div className="flex-1">
-            <h2 className="text-[0.9rem] font-bold">EXPENSE</h2>
-            <h2 className="text-[1.1rem] text-red-700 font-bold">
+            <h2 className="text-[0.9rem] font-bold text-gray-400">EXPENSE</h2>
+            <h2 className="text-[1.1rem] text-red-600 font-bold">
               {exp === 0 ? "$0.00" : "$"+exp+".00"}
             </h2>
           </div>
         </div>
         <div>
-          <h3 className="text-left text-[1rem] font-semibold">History</h3>
+          <h3 className="text-left text-[0.9rem] font-semibold text-gray-400">History</h3>
           <hr className="bg-black h-[2px]" />
-          <div className="flex flex-col text-left my-2 overflow-auto h-[142px] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+          <div style={{}} className={{display: his.length > 0 ? "flex flex-col text-left my-2 overflow-auto h-[130px] scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100 " : "hidden"}}>
             {his &&
               his.map((hip) => {
                 return (
                   <div
                     key={hip.id}
-                    className="bg-white py-2 flex items-center justify-between border-2 mb-1"
+                    className="py-2 flex items-center justify-between border-2 mb-1 glassborder relative"
                   >
                     <div className="flex gap-2 pl-1">
                       <button
-                        className="opacity-2"
+                        className="opacity-2 text-gray-400"
                         onClick={() => filer(hip.id)}
                       >
                         <AiFillDelete />
                       </button>
-                      <span className="text-[1rem]">{hip.name}</span>
+                      <span className="text-[1rem] text-gray-400">{hip.name}</span>
                     </div>
                     <div className="flex gap-1">
-                      <span className="text-[1rem]">
+                      <span className="text-[1rem] text-gray-400 mr-3">
                         {hip.rs < 0 ? hip.rs : "+" + hip.rs}
                       </span>
                       <div
-                        className="w-1 bg-green-500"
+                        className="w-2 bg-green-500 absolute right-0 top-0 h-full"
                         style={{ backgroundColor: hip.add ? "green" : "red" }}
                       ></div>
                     </div>
@@ -115,24 +115,24 @@ function ETracker() {
               })}
           </div>
         </div>
-        <div className="flex flex-col text-left gap-1">
-          <h3 className="text-[1rem] font-semibold">Add new transaction</h3>
+        <div className="flex flex-col text-left gap-2">
+          <h3 className="text-[0.9rem] font-semibold text-gray-400">Add new transaction</h3>
           <hr className="bg-black h-[3px]" />
-          <label className="text-[1rem] font-semibold mt-2">Text</label>
+          <label className="text-[0.9rem] font-semibold mt-2 text-gray-400">Text</label>
           <input
             type="text"
-            className="text-[1rem] p-2"
+            className="text-[1rem] p-2 glassborder text-gray-400"
             placeholder="Enter text..."
             onChange={(e) => settext(e.target.value)}
           />
-          <label className="text-[1rem] font-semibold">
-            Amount <br />
-            (negative-expense,positive-income)
+          <label className="text-[0.9rem] font-semibold text-gray-400">
+            Amount
+            (negative - expense, positive - income)
           </label>
           <input
             placeholder="Enter amount..."
             type="number"
-            className="text-[1rem] p-2 bg-white"
+            className="text-[1rem] p-2 glassborder"
             onChange={(e) => setamt(e.target.value)}
           />
           <button
